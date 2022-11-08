8be6de544fd2c56050757639705f5dabdde67455
Avinash Chourasia <chaurasiaavinash2@gmail.com>
2022-11-08 16:52:10 +0530

    final commit

---
+++ src/app/user-details/user-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent implements OnInit {
+
+  id!: number;
+  user: User = new User;
+  constructor(private route: ActivatedRoute, private usrService: UserService) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.user = new User();
+    this.usrService.getUserById(this.id).subscribe( data => {
+      this.user = data;
+    });
+  }
+
+}
---
+++ src/app/user.ts
@@ -0,0 +1,13 @@
+import { Time } from "@angular/common";
+
+export class User {
+    id!: number;
+    firstName!: string;
+    emailId!: string;
+    from!: Date;
+    timing!: string;
+    spec!: string;
+    consul!: string;
+    status!: string;
+
+}
